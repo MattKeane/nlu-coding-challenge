cbb45bfd79a7624b4f9e59a0ace01107596bb238
Matt Keane <mattpkeane@gmail.com>
2024-10-25 19:33:00 -0500

    adds Feed component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 // component imports
 import HeaderNav from './components/HeaderNav';
 import Slider from './components/Slider';
+import Feed from './components/Feed';
 
 function App() {
   return (
     <div className="App">
       <HeaderNav />
       <Slider />
+      <Feed />
     </div>
   );
 }
---
+++ src/components/Feed/index.js
@@ -0,0 +1,10 @@
+import Card from 'react-bootstrap/Card';
+
+export default function Feed() {
+    return (
+        <Card>
+            <Card.Title>Post Title</Card.Title>
+            <Card.Body>Post body</Card.Body>
+        </Card>
+    );
+}
